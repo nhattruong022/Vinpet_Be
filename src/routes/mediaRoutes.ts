6f9fa18faa3c998450c823fe61938ae2c2cf5ab6
Your Name <your.email@example.com>
2025-11-17 15:38:15 +0700

    remove api not use

--- src/routes/mediaRoutes.ts
+++ src/routes/mediaRoutes.ts
@@ -5,8 +5,6 @@ const router = Router();
 
 // Media routes
 router.post('/upload', MediaController.uploadMiddleware, MediaController.uploadFile);
-router.get('/:filename', MediaController.getMediaInfo);
-router.put('/:filename', MediaController.updateMedia);
 router.delete('/:id', MediaController.deleteMedia);
 
 export default router;
